2a423ac50957be1a39d5d9c6ede6a60dd6118905
Michael DaSilva <programmedmikey@gmail.com>
2016-02-01 20:18:35 -0500

    updated server

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
+var session = require('express-session'); 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection; 
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var multer = require('multer');
 
 var app = express();
 
@@ -19,11 +29,48 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({ 
+  secret:'secret', 
+  saveUninitialized: true, 
+  resave:true
+}));
+
+//passport
+app.use(passport.initialize()); 
+app.use(passport.session());
+
+//validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
+
+var upload = multer({ dest: './uploads' });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
